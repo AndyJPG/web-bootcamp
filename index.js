fe6155c89a4485c871bdb7b2aa054ae55d3166dd
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2019-12-30 22:04:31 +1100

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("linked");
+let changeBackgroundButton = document.querySelector("button");
+let websiteBody = document.querySelector("body");
+
+changeBackgroundButton.addEventListener("click", () => {
+    if (websiteBody.style.background == "white") {
+        websiteBody.style.background = "pink";
+    } else {
+        websiteBody.style.background = "white";
+    }
+})
