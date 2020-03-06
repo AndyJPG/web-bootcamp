c26144a7ca3f973f4d327ce12156802e983d6294
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2020-03-06 19:57:09 +1100

    finish yelp camp with database implementation

--- back-end-practice/YelpCamp/app.js
+++ back-end-practice/YelpCamp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const campgrounds = [
     {name: 'Salmon Creek', image: 'https://paperbarkcamp.com.au/wp-content/uploads/2019/07/paperbark_flash-camp_news_1218x650.jpg'},
@@ -12,27 +13,72 @@ const campgrounds = [
     {name: 'Cherry Hill', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=949&q=80'}
 ];
 
+mongoose.connect("mongodb://127.0.0.1:27017/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Box Hill",
+//     image: 'https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2747&q=80',
+//     description: "Beautiful Boxhil campground "
+// }, (err, campground) => {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("New campground");
+//         console.log(campground);
+//     }
+// });
 
 app.get('/', (req, res) => {
     res.render("landing");
 });
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find({}, (err, campgrounds) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('index', {campgrounds: campgrounds});
+        }
+    });
+
 });
 
 app.post('/campgrounds', (req, res) => {
-    const { name, imageUrl } = req.body;
-    const newCampGround = {name: name, image: imageUrl};
-    campgrounds.push(newCampGround);
+    const { name, imageUrl, description } = req.body;
+    const newCampGround = {name: name, image: imageUrl, description: description};
 
-    res.redirect('/campgrounds');
+    Campground.create(newCampGround, (err, newCamp) => {
+       if(err) {
+           console.log(err);
+       } else {
+           res.redirect("/campgrounds");
+       }
+    });
 });
 
 app.get("/campgrounds/new", (req, res) => {
    res.render('new');
+});
+
+app.get("/campgrounds/:id", (req, res) => {
+    Campground.findById(req.params.id, (err, camp) => {
+       if(err) {
+           console.log(err);
+       } else {
+           res.render("show", {campground: camp});
+       }
+    });
 });
 
 app.listen(port, () => {
